9000e660f4b74a2b58ab6d2cb875e35780159211
simba-pumba <akakak82@sookmyung.ac.kr>
2020-04-13 21:56:19 +0900

    200413

---
+++ GraphSAGE/layer.py
@@ -0,0 +1,14 @@
+"""
+layers.py
+"""
+import torch
+import torch.nn as nn
+
+
+class maxAggregateLayer(nn.Module):
+    def __init__():
+        super(maxAggregateLayer, self).__init__()
+        pass
+
+    def forward(self):
+    	pass
