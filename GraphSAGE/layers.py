ee066342a42b5487ef56d3f751c86939d247d14d
simba-pumba <akakak82@sookmyung.ac.kr>
2020-04-19 18:48:40 +0900

    200419

--- GraphSAGE/layers.py
+++ GraphSAGE/layers.py
@@ -7,29 +7,27 @@ import torch.nn.functional as F
 
 # YJ
 class poolAggregateLayer(nn.Module):
-    def __init__(self, input_dim, output_dim, activation, hidden_dim, bias = True, normalize = True):
+    def __init__(self, input_dim, output_dim, activation, bias = True, normalize = True):
         super().__init__()
-        self.fc_self = nn.Linear(input_dim, output_dim, bias = False )
-        self.activation = activation
+        self.fc_self = nn.Linear(input_dim, output_dim, bias = False)
        # self.drop = nn.Dropoutd(drop_rate)
-        nn.init.xavier_uniform(self.pool, gain=nn.init.calculate_gain(activation)) 
+        nn.init.xavier_uniform(self.fc_self, gain=nn.init.calculate_gain(activation)) 
+
+    
         self.fc_pool = nn.Linear(input_dim, output_dim, bias = bias)
-        self.activation = {sigmoid:F.sigmoid, relu:F.relu}[activation]
+        nn.init.xavier_uniform(self.pool, gain=nn.init.calculate_gain(activation)) 
+        self.activation = {'sigmoid':F.sigmoid,'relu':F.relu}[activation]
         self.combine = F.max
         self.norm = F.nomalize
 
 
     def forward(self, i, neibs):
         dst = self.fc_self(i)
-        dst = self.activation(source)
-
+        dst = self.activation(dst)
+ 
         src = self.fc_pool(neibs)
         src = self.activation(src)
         
-        # if not self.concat:
-        #     output = tf.add_n([from_self, from_neighs])
-        # else:
-        #     output = tf.concat([from_self, from_neighs], axis=1)
         out = self.combine( dst + src )
         out = self.norm(out)
         return out 
